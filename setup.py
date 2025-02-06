acbbb41771172df37be0d87b3c275ccec0da6732
webtechmoz <zoidycine@gmail.com>
2025-02-06 17:27:13 +0200

    pyflet 0.1.0 updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyflet',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(include=['pyflet', 'pyflet.*']),
     install_requires=[
         'typer',
     ],
